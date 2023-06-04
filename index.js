970231d419f3e7b213c75bb891a2d6fba16cc615
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-06-03 20:54:59 -0400

    added packages needed for this applications as well as an array of questions for user input

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
+// const path = require('path');
+// const {log} = require('console');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub user name?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+      {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Please write a short description for your project.',
+      },
+      {
+        type: 'list',
+        name: 'projectLicense',
+        message: 'What license should your project have?',
+        choices: ['MIT', 'GPL', 'Apache', 'MPL', 'Unlicensed'],
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
